c8e19b7f88db0865996da915c201ebb59587d969
Abraham Ayamigah <abkc070@gmail.com>
2024-03-24 16:54:02 -0700

    5. Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,50 +1,67 @@
 #!/usr/bin/python3
 """A script that starts a Flask web application"""
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def hello():
     """
     Returns a greeting message.
     """
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
     Returns 'HBNB'.
     """
-    return 'HBNB'
+    return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
+def c_text(text):
     """
     Returns a string with 'C' followed by the provided text.
-    Replaces underscores with spaces in the text.
+    Underscores in the text are replaced with spaces.
     """
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+
+@app.route(
+    '/python/',
+    defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text):
+def python_text(text):
     """
     Returns a string with 'Python' followed by the provided text.
-    Replaces underscores with spaces in the text.
-    If no text is provided, uses 'is cool' as the default text.
+    Underscores in the text are replaced with spaces.
+    If no text is provided, the default text 'is cool' is used.
     """
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """
-    Returns a string stating that the provided number is a number.
+    Returns a string indicating that the provided number is a number.
     """
     return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """
+    Renders an HTML template with the provided number.
+    The number is passed to the template as a variable 'n'.
+    """
+    return render_template('5-number.html', n=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
