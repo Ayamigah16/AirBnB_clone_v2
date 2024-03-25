b84182446b6ee08e97e4c8847712e426e4c89e2d
Abraham Ayamigah <122880578+Ayamigah16@users.noreply.github.com>
2024-03-25 03:09:52 +0000

    Update state.py

--- models/state.py
+++ models/state.py
@@ -11,19 +11,20 @@ class State(BaseModel, Base):
 
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship(
-        "City",
-        back_populates="state", cascade="all, delete-orphan")
 
-    def to_dict(self):
-        """Return a dictionary representation of the object."""
-        obj_dict = super().to_dict()
-        obj_dict['__class__'] = type(self).__name__
-        obj_dict['name'] = self.name
-        # Add any other state-specific attributes
-        return obj_dict
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+        cities = relationship(
+            "City",
+            back_populates="state", cascade="all, delete-orphan")
 
-    if os.getenv(HBNB_TYPE_STORAGE) != 'db':
+        def to_dict(self):
+            """Return a dictionary representation of the object."""
+            obj_dict = super().to_dict()
+            obj_dict['__class__'] = type(self).__name__
+            obj_dict['name'] = self.name
+            # Add any other state-specific attributes
+            return obj_dict
+
         @property
         def cities(self):
             """Returns the list of City instances
