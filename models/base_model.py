847664edcb1672afc1281508390b4cdbb7e81dc3
Abraham Ayamigah <122880578+Ayamigah16@users.noreply.github.com>
2024-03-25 03:58:35 +0000

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialize a new instance of a BaseMOdel."""
-         if not kwargs:
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
