d3271c185b829ff7056dd587144ab511187e6358
Abraham Ayamigah <122880578+Ayamigah16@users.noreply.github.com>
2024-03-25 03:57:41 +0000

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -18,8 +18,11 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialize a new instance of a BaseMOdel."""
-        # checking if key passed is not "__class__"
-        if kwargs:
+         if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key == "created_at" or key == 'updated_at':
@@ -27,8 +30,21 @@ class BaseModel:
                             setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
                         else:
                             setattr(self, key, value)
+                    elif key != "id":
+                        self.id = str(uuid.uuid4())                    
                     else:
-                        setattr(self, key, value)        
+                        setattr(self, key, value)
+        # checking if key passed is not "__class__"
+        # if kwargs:
+        #     for key, value in kwargs.items():
+        #         if key != '__class__':
+        #             if key == "created_at" or key == 'updated_at':
+        #                 if isinstance(value, str):
+        #                     setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+        #                 else:
+        #                     setattr(self, key, value)
+        #             else:
+        #                 setattr(self, key, value)        
         
         # """Class constructor"""
         # self.id = str(uuid.uuid4())
