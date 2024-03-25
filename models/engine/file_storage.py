15c234bd9d1d0c1be8bd9f8e556dd45b49e9519c
Abraham Ayamigah <122880578+Ayamigah16@users.noreply.github.com>
2024-03-25 02:41:44 +0000

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -35,6 +35,12 @@ class FileStorage:
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
         try:
             with open(
                     FileStorage.__file_path,
